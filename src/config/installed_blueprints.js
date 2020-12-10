a3e3a2cd1f5b274edfb7ababe976fdbc0c4d3a8d
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-12-10 06:40:28 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,8 +1,10 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'HomeScreen4184048', human_name: ‘Home’, access_route: 'HomeScreen4184048'},
-{ name: 'PrivacyPolicyScreen6184046', human_name: ‘Privacy Policy’, access_route: 'PrivacyPolicyScreen6184046'},
-{ name: 'TermsScreen5184047', human_name: ’Terms & Conditions’, access_route: 'TermsScreen5184047'},
+
+  { name: 'HomeScreen4184048', human_name: 'Home', access_route: 'HomeScreen4184048' },
+  { name: 'PrivacyPolicyScreen6184046', human_name: ‘Privacy Policy’, access_route: 'PrivacyPolicyScreen6184046' },
+  { name: 'TermsScreen5184047', human_name: ’Terms & Conditions’, access_route: 'TermsScreen5184047' },
+  
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
